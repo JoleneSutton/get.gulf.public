607562e2281e0f1b4c6acbf1da4d8a92ffb4f6f9
Jolene Sutton <jolene.sutton@gmail.com>
2025-02-14 07:09:12 -0400

    update function documentation

--- R/get_species_codes.R
+++ R/get_species_codes.R
@@ -3,10 +3,11 @@
 #' @param pattern The pattern to search for. Case insensitive. Use "^" for "begins with", and "$" for "ends with". See examples.
 #' @param field The field (column), in which to search for the pattern
 #' @examples
-#' get_species_codes('^snow crab','english')
-#' get_species_codes('^10$','code')
-#' get_species_codes('^130$','statac_code')
-#' get_species_codes('morue','french')
-#' get_species_codes('^morue','french')
+#' #library(gulf)
+#' #get_species_codes('^snow crab','english')
+#' #get_species_codes('^10$','code')
+#' #get_species_codes('^130$','statac_code')
+#' #get_species_codes('morue','french')
+#' #get_species_codes('^morue','french')
 #' @export
 get_species_codes<-function(pattern,field){}
